e9292abf84fc768e6afff09f8d89f931f289f21d
Daniel Reina <drrmovil@gmail.com>
2020-11-23 16:19:43 +0100

    testeando modelo shop

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -6,13 +6,16 @@ const User = require("../models/user.model")
 const ensureAuthenticated = (req, res, next) => req.isAuthenticated() ? next() : res.render('auth/login', { errorMsg: 'Desautorizado, inicia sesión' })
 const checkRole = admittedRoles => (req, res, next) => admittedRoles.includes(req.user.role) ? next() : res.render('auth/login', { errorMsg: 'Desautorizado, no tienes permisos' })
 
-router.get('/', ensureAuthenticated, checkRole(['NORMAL', 'ADMIN']),(req, res) => {
-    
-    res.render('profile', { user: req.user })
-  
+router.get('/', ensureAuthenticated, checkRole(['NORMAL', 'ADMIN', 'SHOP']),(req, res) => {
+    if (req.user.role === 'NORMAL' || req.user.role === 'ADMIN') {
+        res.render('profile/user-profile', { user: req.user })
+    }
+     if (req.user.role === 'SHOP') {
+        res.render('profile/shop-profile', { user: req.user })
+    }
     })
 
-router.get('/avatar', ensureAuthenticated, checkRole(['NORMAL', 'ADMIN']), (req, res) => {
+router.get('/avatar', ensureAuthenticated, checkRole(['NORMAL', 'ADMIN', 'SHOP']), (req, res) => {
        
     res.render('avatar', { user: req.user })
    
@@ -20,11 +23,12 @@ router.get('/avatar', ensureAuthenticated, checkRole(['NORMAL', 'ADMIN']), (req,
 })
     
 router.post('/avatar', CDNupload.single('imageFile'), (req, res) => {
+  
 
     User
         .findByIdAndUpdate(req.user.id, {
             avatar: {
-                imageName: req.user.avatar.imageName,
+                imageName: req.body.imageName,
                 path: req.file.path,
                 originalName: req.file.originalname
             }           
