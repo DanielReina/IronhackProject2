f15fa285aa3cc81986eafc207527512b9dcf4b45
Daniel Reina <drrmovil@gmail.com>
2020-11-24 16:02:56 +0100

    restaurando

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,19 +1,64 @@
+let mapInstance
 
-function drawMap(){
+function initApp() {
+
+  
     
-  const mapInstance = new google.maps.Map(document.querySelector('#map'), {
-    center: {
-      lat: 37.381884,
-      lng: -5.987307
-    }, zoom: 14
+  getUsersFromApi()
+}
+    
+function drawMap(user) {
+    let center = { lat: user.location.coordinates[0], lng: user.location.coordinates[1] }
+    mapInstance = new google.maps.Map(document.querySelector('#map'), {
+        center, zoom: 14
 
-  })
+    })
 
 }
 
 
-    new google.maps.Marker({
-        map: mapInstance,
-        position: directions.ironhackBCN.coords,
-        title: directions.ironhackBCN.title
-    })
+class PlacesApiHandler {
+
+    constructor() {
+
+        console.log('API handler inicializada')
+
+        this.axiosApp = axios.create({
+            baseURL: 'http://localhost:3000/api/usuarios'
+        })
+    }
+ getOneUser = placeId => this.axiosApp.get(`/${placeId}`)
+
+}
+
+const apiHandler = new PlacesApiHandler()
+
+function getUsersFromApi() {
+    let id = document.querySelector('#map').getAttribute('userId')
+    console.log(id)
+    
+    apiHandler
+      .getOneUser(id)
+      .then(response => {
+          console.log(id)
+              drawMap(response.data)
+        drawMarkers(response.data)
+      })
+      .catch(err => console.log(err))
+}
+
+function drawMarkers(user) {
+
+    console.log(mapInstance)
+
+        let position = { lat: user.location.coordinates[0], lng: user.location.coordinates[1] }
+
+        new google.maps.Marker({
+            map: mapInstance,
+            position,
+            title: user.name
+        })
+  
+
+    mapInstance.setCenter({ lat: users.location.coordinates[0], lng: users.location.coordinates[1] })
+}
